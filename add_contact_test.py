079ccebb59a3ed7de530a56696267c5f3394d9db
Marina A <m.abrashkina2015@gmail.com>
2020-09-28 12:38:59 +0300

    Task#3 lesson#1

---
+++ add_contact_test.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest
+
+from contact import Contact
+
+
+class AddContact(unittest.TestCase):
+    def setUp(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+
+    def test_add_new_contact(self):
+        wd = self.wd
+        self.open_application(wd)
+        self.login(wd, "admin", "secret")
+        self.create_contact(wd, Contact("FirstName", "LastName", "Company", "Telephone"))
+        self.return_to_home(wd)
+        self.logout(wd)
+
+
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_home(self, wd):
+        wd.find_element_by_link_text("home").click()
+
+    def create_contact(self, wd, contact):
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
+        wd.find_element_by_name("company").clear()
+        wd.find_element_by_name("company").send_keys(contact.company)
+        wd.find_element_by_name("home").clear()
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
+        # submit contact creation
+        wd.find_element_by_name("submit").click()
+
+
+    def login(self, wd, username, password):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(username)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_application(self, wd):
+        # open application
+        wd.get("http://localhost/addressbook/")
+
+    def tearDown(self):
+        self.wd.quit()
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ contact.py
@@ -0,0 +1,8 @@
+
+
+class Contact:
+    def __init__(self,first_name,last_name,company,home_phone):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.company = company
+        self.home_phone = home_phone
