f0fdd40ba88fa9153f7a6ee5debbf02c3ca2d5a4
Marina A <m.abrashkina2015@gmail.com>
2020-09-29 11:04:17 +0300

    HW Task#07 Lesson#2

--- fixture/session.py
+++ fixture/session.py
@@ -1,4 +1,6 @@
-
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 class SessionHelper:
 
     def __init__(self,app):
@@ -16,4 +18,10 @@ class SessionHelper:
 
     def logout(self):
         wd = self.app.wd
+        #wd.find_element_by_link_text("Logout").click()
+        wd.implicitly_wait(5)
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_link_text("Logout").click()
+        #logout_button = WebDriverWait(wd, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "Logout")))
+        #logout_button.click();
+        #wd.find_element_by_xpath("//form[@name='logout']/a").click()
