78870bdaac520ca23106593ddb2dec5952c1a6be
Marina A <m.abrashkina2015@gmail.com>
2020-10-29 16:37:56 +0200

    lesson#5

--- fixture/session.py
+++ fixture/session.py
@@ -32,7 +32,11 @@ class SessionHelper:
 
     def is_logged_in_as(self,username):
         wd = self.app.wd
-        return wd.find_element_by_xpath("//form[@name='logout']/b").text=="("+username+")"
+        return self.get_loged_user() == "username"
+
+    def get_loged_user(self):
+        wd = self.app.wd
+        return wd.find_element_by_xpath("//form[@name='logout']/b").text[1:-1]
 
     def ensure_login(self,username,password):
         wd = self.app.wd
