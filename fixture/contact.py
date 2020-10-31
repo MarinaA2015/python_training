25ebe5acb7091e3bb964ed7bcd85de63de595976
Marina A <m.abrashkina2015@gmail.com>
2020-10-31 14:28:43 +0200

    lesson#5. All changes from the lesson#5 was done

--- fixture/contact.py
+++ fixture/contact.py
@@ -1,4 +1,5 @@
 from model.contact import Contact
+import re
 
 class ContactHelper:
     def __init__(self, app):
@@ -84,7 +85,41 @@ class ContactHelper:
             self.contact_cache = []
             for el in wd.find_elements_by_name("entry"):
                 id = el.find_element_by_name("selected[]").get_attribute("value")
-                name = el.find_element_by_xpath(".//td[3]").text
+                first_name = el.find_element_by_xpath(".//td[3]").text
                 last_name = el.find_element_by_xpath(".//td[2]").text
-                self.contact_cache.append(Contact(id=id, first_name=name, last_name=last_name))
+                all_phones = el.find_element_by_xpath(".//td[6]").text
+                self.contact_cache.append(Contact(id=id, first_name=first_name, last_name=last_name,
+                                                  all_phones_from_contact_page=all_phones))
         return self.contact_cache
+
+    def get_contact_info_from_edit_page(self, index):
+        wd = self.app.wd
+        self.select_contact_by_index(index)
+        first_name = wd.find_element_by_name("firstname").get_attribute("value")
+        last_name = wd.find_element_by_name("lastname").get_attribute("value")
+        id = wd.find_element_by_name("id").get_attribute("value")
+        home_phone = wd.find_element_by_name("home").get_attribute("value")
+        mobile_phone = wd.find_element_by_name("mobile").get_attribute("value")
+        work_phone = wd.find_element_by_name("work").get_attribute("value")
+        second_phone = wd.find_element_by_name("phone2").get_attribute("value")
+        self.open_home()
+        return Contact(id=id, first_name=first_name, last_name=last_name,  home_phone=home_phone,
+                       mobile_phone=mobile_phone, work_phone=work_phone, second_phone=second_phone)
+
+
+
+    def get_contacts_from_view_page(self, index):
+        wd = self.app.wd
+        self.open_contact_view_by_index(index)
+        text = wd.find_element_by_id("content").text
+        home_phone = re.search("H: (.*)", text).group(1)
+        work_phone = re.search("W: (.*)", text).group(1)
+        mobile_phone = re.search("M: (.*)", text).group(1)
+        second_phone = re.search("P: (.*)", text).group(1)
+        self.open_home()
+        return Contact(home_phone=home_phone,
+                       mobile_phone=mobile_phone, work_phone=work_phone, second_phone=second_phone)
+
+    def open_contact_view_by_index(self, index):
+        wd = self.app.wd
+        wd.find_elements_by_xpath("//img[@alt='Details']")[index].click()
--- model/contact.py
+++ model/contact.py
@@ -2,12 +2,17 @@ from sys import maxsize
 
 
 class Contact:
-    def __init__(self,first_name=None,last_name=None,company=None,home_phone=None,id=None):
+    def __init__(self, first_name=None,last_name=None,company=None,home_phone=None, mobile_phone=None,
+                 work_phone=None, second_phone=None, id=None, all_phones_from_contact_page=None):
         self.first_name = first_name
         self.last_name = last_name
         self.company = company
         self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.second_phone = second_phone
         self.id = id
+        self.all_phones_from_contact_page = all_phones_from_contact_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.first_name, self.last_name)
@@ -21,3 +26,7 @@ class Contact:
             return int(self.id)
         else:
             return maxsize
+
+
+
+
