d38d7ac7a8311998489c2cda33c0127c44116634
Marina A <m.abrashkina2015@gmail.com>
2020-11-21 17:39:05 +0200

    Lesson#6 Task#18 (corrections)

--- generator/contact.py
+++ generator/contact.py
@@ -5,6 +5,7 @@ import os.path
 import json
 import getopt
 import sys
+import jsonpickle
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of contacts", "file"])
@@ -36,4 +37,5 @@ testdata = [Contact(first_name="", last_name="", company="", home_phone="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__,indent=2))
+    #out.write(json.dumps(testdata, default=lambda x: x.__dict__,indent=2))
+    out.write(json.dumps(testdata, default=lambda x: jsonpickle.encode(x, unpicklable=False), indent=2))
