e5329064963a1278790998b3f88d877feb6f3f8f
Marina A <m.abrashkina2015@gmail.com>
2020-11-21 17:33:45 +0200

    Lesson#6 Task#18 (corrections)

--- generator/group.py
+++ generator/group.py
@@ -5,6 +5,7 @@ import os.path
 import json
 import getopt
 import sys
+import jsonpickle
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
@@ -13,7 +14,7 @@ except getopt.GetoptError as err:
     sys.exit(2)
 
 
-n = 5
+n = 3
 f = "data/groups.json"
 
 for o, a in opts:
@@ -35,4 +36,5 @@ testdata = [Group(name="", header="", footer="")] + [
 file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
 
 with open(file, "w") as out:
-    out.write(json.dumps(testdata, default=lambda x: x.__dict__,indent=2))
+    #out.write(json.dumps(testdata, default=lambda x: x.__dict__,indent=2))
+    out.write(json.dumps(testdata, default=lambda x: jsonpickle.encode(x, unpicklable=False), indent=2))
--- test/add_group_test.py
+++ test/add_group_test.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
-
-
 from model.group import Group
-import pytest
-from data.add_group import constant as testdata
 
 
-@pytest.mark.parametrize("group", testdata,ids=[repr(x) for x in testdata])
-def test_add_new_group(app,group):
+def test_add_group(app, data_groups):
+    group = data_groups
     old_groups = app.group.get_groups_list()
     #group_new = Group("GroupName", "GroupHeader", "GroupFooter")
     app.group.create(group)
